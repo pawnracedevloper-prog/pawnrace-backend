9543a61c90186b43c1ed48ca5387e538017e688a
Satyajit-tech2006 <satyajitswain2006@gmail.com>
2025-12-23 20:34:26 +0530

    fix: correct import statement formatting in chat controller

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -1,5 +1,5 @@
-import { asyncHandler } from "../utils/asyncHandler.js";
-import { ApiResponse } from "../utils/ApiResponse.js";
+import asyncHandler  from "../utils/asyncHandler.js";
+import ApiResponse  from "../utils/ApiResponse.js";
 import { Message } from "../models/message.model.js";
 import { Course } from "../models/course.model.js";
 
